25b53a9237c586c0bfb7ea723bdd2c6678b79b85
enlore <n.e.lorenson@gmail.com>
2014-01-09 00:25:48 -0600

    Gmaps in the ui

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
+function mapInit() {
+var mapOpts = {
+    center: new google.maps.LatLng(36.5290495, -87.35296590000002),
+    zoom: 14
+}
 
+var map = new google.maps.Map(document.getElementById("gmap"),
+    mapOpts)
+}
+
+google.maps.event.addDomListener(window, 'load', mapInit)
