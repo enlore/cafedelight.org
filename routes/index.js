f1354de3448c23a5e9c888a90e37c293abe50647
enlore <n.e.lorenson@gmail.com>
2014-01-16 17:51:41 -0600

    Title

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@
  */
 
 exports.index = function(req, res){
-    res.render('index', { title: 'Cafe de Light - Espresso and Frappes by APSU', page_name: 'index' });
+    res.render('index', { page_name: 'index' });
 };
 
 exports.menu = function (req, res) {
-    res.render('menu', { title: 'Cafe de Light - Menu', page_name: 'menu' })
+    res.render('menu', { page_name: 'menu' })
 }
